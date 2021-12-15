2cd9ec4b58e5f9fd117e042e2c5a54263405c9d1
KlausSeer <33689728+KlausSeer@users.noreply.github.com>
2021-12-15 11:55:01 -0500

    First Commit (Seek, Flee and Arrival)
    -Added Test Scene
    -Added Seek Behavior
    -Added Flee Behavior
    -Added Arrival Behavior

---
+++ Assets/Scripts/Steering.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Behavior
+{
+    NONE,
+    SEEK,
+    FLEE,
+    ARRIVAL,
+}
+
+public class Steering : MonoBehaviour
+{
+
+    [Range(2.0f, 7.0f)]
+    [SerializeField]
+    private float speed = 5.0f;
+
+    [Range(0.0f, 1.0f)]
+    [SerializeField]
+    private float slowDownRadius = 1.0f;
+
+    
+    [SerializeField]
+    private Behavior behavior;
+
+    private Vector2 velocity = Vector2.zero;
+
+    #region Behavoiurs
+
+    Vector2 Seek(Vector2 desiredVelocity)
+    {
+        return desiredVelocity - velocity;
+    }
+
+    #endregion
+
+    void Update()
+    {
+        Vector2 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 targetDistance = (targetPosition - (Vector2)transform.position);
+
+        Vector2 desiredVelocity = targetDistance.normalized * speed;
+
+        Vector2 steering = Vector2.zero;
+
+        #region switch
+
+        switch (behavior)
+        {
+            case Behavior.NONE:
+                velocity = desiredVelocity;
+                break;
+            case Behavior.SEEK:
+                steering = Seek(desiredVelocity);
+                velocity += steering * Time.deltaTime;
+                break;
+            case Behavior.FLEE:
+                steering = Seek(-desiredVelocity);
+                velocity += steering * Time.deltaTime;
+                break;
+            case Behavior.ARRIVAL:
+                steering = Seek(desiredVelocity);
+
+                float slowDownFactor = Mathf.Clamp01(targetDistance.magnitude / slowDownRadius);
+                velocity *= slowDownFactor;
+                velocity += steering * Time.deltaTime;
+
+                break;
+            default:
+                break;
+        }
+
+        #endregion
+
+        transform.position += (Vector3)velocity * Time.deltaTime;
+    }
+}
